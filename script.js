55d390effcfa20cecde14fcdec29e8ccc7356949
Hari <harishnathr@gmail.com>
2023-09-18 17:56:17 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,39 @@
+var hole = $(".hole");
+var count = 0;
+var stop = false;
+var start = false;
+function random() {
+    var n = (Math.floor(Math.random() * 9)) + 1;
+    $(".h" + n).html('<img src="./img/rabbit.png" alt="" class="rab-img">');
+    $(".h" + n).addClass("active");
+}
+function remove() {
+    var act = $(".active");
+    act.removeClass("active");
+    act.html('');
+}
+$(document).keypress(function () {
+    start = true;
+    $(".result").text('');
+});
+
+setInterval(function () {
+    if (stop === false && start === true) {
+        remove();
+        random();
+    }
+
+}, 400);
+
+
+
+hole.click(function () {
+    count++;
+    if ($(this).hasClass("active")) {
+        $(".result").text("You Caught Me!");
+        $(this).html('<img src="./img/caught rabbit.png" alt="" class="rab-img">');
+        stop = true;
+    }
+    $(".attempts-number").text(count);
+});
+
